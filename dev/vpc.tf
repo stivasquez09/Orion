5c85f91a38aded8fbe35776002cf66fee20cfcc5
stivasquez09 <stivasquez09@gmail.com>
2026-01-30 22:06:15 -0500

    vpc down

--- dev/vpc.tf
+++ dev/vpc.tf
@@ -1,29 +1,29 @@
-resource "aws_vpc" "main1" {
-  cidr_block           = "10.10.0.0/16"
-  enable_dns_hostnames = true
-  enable_dns_support   = true
+# resource "aws_vpc" "main1" {
+#   cidr_block           = "10.10.0.0/16"
+#   enable_dns_hostnames = true
+#   enable_dns_support   = true
 
-  tags = {
-    Name = "VPC1"
-  }
-}
-resource "aws_subnet" "subnet1" {
-  vpc_id                  = aws_vpc.main1.id
-  cidr_block              = "10.10.1.0/24"
-  availability_zone       = "us-east-1a"
-  map_public_ip_on_launch = true
+#   tags = {
+#     Name = "VPC1"
+#   }
+# }
+# resource "aws_subnet" "subnet1" {
+#   vpc_id                  = aws_vpc.main1.id
+#   cidr_block              = "10.10.1.0/24"
+#   availability_zone       = "us-east-1a"
+#   map_public_ip_on_launch = true
 
-  tags = {
-    Name = "subnet1_public"
-  }
-}
-resource "aws_subnet" "subnet2" {
-  vpc_id                  = aws_vpc.main1.id
-  cidr_block              = "10.10.2.0/24"
-  availability_zone       = "us-east-1b"
-  map_public_ip_on_launch = true
+#   tags = {
+#     Name = "subnet1_public"
+#   }
+# }
+# resource "aws_subnet" "subnet2" {
+#   vpc_id                  = aws_vpc.main1.id
+#   cidr_block              = "10.10.2.0/24"
+#   availability_zone       = "us-east-1b"
+#   map_public_ip_on_launch = true
 
-  tags = {
-    Name = "subnet2_private"
-  }
-}
+#   tags = {
+#     Name = "subnet2_private"
+#   }
+# }
