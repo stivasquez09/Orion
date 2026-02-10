2bddd6236e3a505d523e13c75dc7e91071a3cb0d
stivasquez09 <stivasquez09@gmail.com>
2026-02-09 19:20:51 -0500

    backend

--- dev/backend.tf
+++ dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = dev-403455006325-infracloud-s3-bucket             # Nombre del bucket donde se guardará el state
+    bucket = "dev-403455006325-infracloud-s3-bucket"            # Nombre del bucket donde se guardará el state
     key    = "dev/terraform.tfstate" # Ruta/nombre del archivo state dentro del bucket
     region = "us-east-1"                      # Región del bucket
     encrypt = true                           
