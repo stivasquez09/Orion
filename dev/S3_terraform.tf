e4a237e7001c29088bdbd91c02f5d7de7cd78892
stivasquez09 <stivasquez09@gmail.com>
2026-02-10 19:16:38 -0500

    carpetas para bucket s3

--- dev/S3_terraform.tf
+++ dev/S3_terraform.tf
@@ -1,8 +1,8 @@
 module "s3_bucket" {
-  source = "git::https://github.com/terraform-aws-modules/terraform-aws-s3-bucket.git"
-  bucket = "${var.environment}-${data.aws_caller_identity.current.account_id}-infracloud-s3-bucket"
+  source        = "git::https://github.com/terraform-aws-modules/terraform-aws-s3-bucket.git"
+  bucket        = "${var.environment}-${data.aws_caller_identity.current.account_id}-infracloud-s3-bucket"
   create_bucket = false
-  
+
   versioning = {
     enabled = true
   }
@@ -17,4 +17,21 @@ module "s3_bucket" {
       }
     }
   ]
+}
+
+locals {
+  base_path = "env=${var.environment}/app=${var.application}"
+  folders = [
+    "${local.base_path}/",
+    "${local.base_path}/year=${formatdate("YYYY", timestamp())}/",
+    "${local.base_path}/year=${formatdate("YYYY", timestamp())}/month=${formatdate("MM", timestamp())}/"
+  ]
+}
+
+resource "aws_s3_object" "folders" {
+  for_each = toset(local.folders)
+
+  bucket  = module.s3_bucket.s3_bucket_id
+  key     = each.value
+  content = ""
 }
--- dev/data.tf
+++ dev/data.tf
@@ -1,6 +1,11 @@
 data "aws_caller_identity" "current" {}
 variable "environment" {
-    default = "DEV"
-    description = "etiqueta para ambiente"
-  
+  default     = "DEV"
+  description = "etiqueta para ambiente"
+
+}
+
+variable "application" {
+  default       = "Orion"
+  description = "Carpetas para bucket Orion"
 }
