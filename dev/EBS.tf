e6162fb5c7a1081a21f548b4b701f52c9a9a44d4
stivasquez09 <stivasquez09@gmail.com>
2026-06-09 21:24:06 -0500

    new aws config + remedetion

--- dev/EBS.tf
+++ dev/EBS.tf
@@ -1,19 +1,19 @@
-resource "aws_ebs_volume" "ejemplo_ebs" {
-  availability_zone = "us-east-1a" # Reemplaza con tu zona de disponibilidad
-  size              = 10           # Tamaño en GB
-  type              = "gp3"        # Tipo de volumen (gp3, gp2, io1, etc.)
+# resource "aws_ebs_volume" "ejemplo_ebs" {
+#   availability_zone = "us-east-1a" # Reemplaza con tu zona de disponibilidad
+#   size              = 10           # Tamaño en GB
+#   type              = "gp3"        # Tipo de volumen (gp3, gp2, io1, etc.)
 
-  tags = {
-    Name = "Volumen-EBS-Terraform"
-  }
-}
+#   tags = {
+#     Name = "Volumen-EBS-Terraform"
+#   }
+# }
 
-resource "aws_ebs_volume" "ejemplo_ebs1" {
-  availability_zone = "us-east-1a" # Reemplaza con tu zona de disponibilidad
-  size              = 20           # Tamaño en GB
-  type              = "gp3"        # Tipo de volumen (gp3, gp2, io1, etc.)
+# resource "aws_ebs_volume" "ejemplo_ebs1" {
+#   availability_zone = "us-east-1a" # Reemplaza con tu zona de disponibilidad
+#   size              = 20           # Tamaño en GB
+#   type              = "gp3"        # Tipo de volumen (gp3, gp2, io1, etc.)
 
-  tags = {
-    Name = "Volumen-EBS-Terraform1"
-  }
-}
+#   tags = {
+#     Name = "Volumen-EBS-Terraform1"
+#   }
+# }
