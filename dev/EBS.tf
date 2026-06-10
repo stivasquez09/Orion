8dab0fcfe5abeaa8319e5f8cb6de039fca9342de
stivasquez09 <stivasquez09@gmail.com>
2026-06-09 19:03:49 -0500

    new ebs

---
+++ dev/EBS.tf
@@ -0,0 +1,9 @@
+resource "aws_ebs_volume" "ejemplo_ebs" {
+  availability_zone = "us-east-1a" # Reemplaza con tu zona de disponibilidad
+  size              = 10           # Tamaño en GB
+  type              = "gp3"        # Tipo de volumen (gp3, gp2, io1, etc.)
+
+  tags = {
+    Name = "Volumen-EBS-Terraform"
+  }
+}
