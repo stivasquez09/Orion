cf5ae1e131bd145e55192f7df6faa04420bfa552
stivasquez09 <stivasquez09@gmail.com>
2026-06-09 19:25:42 -0500

    new aws config + remedetion

--- dev/aws_config.tf
+++ dev/aws_config.tf
@@ -19,74 +19,88 @@ resource "aws_iam_role" "remediation" {
     }]
   })
 }
-
 resource "aws_iam_role_policy" "remediation" {
   role = aws_iam_role.remediation.name
   name = "ebs-delete-policy"
 
   policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [{
-      Effect   = "Allow"
-      Action   = ["ec2:DeleteVolume", "ec2:DescribeVolumes"]
-      Resource = "*"
-    }]
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = ["ec2:DeleteVolume", "ec2:DescribeVolumes"]
+        Resource = "*"
+      },
+      {
+        Effect   = "Allow"
+        Action   = "iam:PassRole"
+        Resource = "*"
+      },
+      {
+        Effect   = "Allow"
+        Action   = [
+          "ssm:StartAutomationExecution",
+          "ssm:GetAutomationExecution"
+        ]
+        Resource = "*"
+      }
+    ]
   })
 }
 
-# ─────────────────────────────────────────────
-# CONFORMANCE PACK
-# ─────────────────────────────────────────────
-resource "aws_config_conformance_pack" "ebs_unused" {
-  name = "ebs-unused-volumes-pack"
+# # ─────────────────────────────────────────────
+# # CONFORMANCE PACK
+# # ─────────────────────────────────────────────
+# resource "aws_config_conformance_pack" "ebs_unused" {
+#   name = "ebs-unused-volumes-pack"
 
-  template_body = <<-EOT
-    Parameters:
-      RemediationRoleArn:
-        Type: String
+#   template_body = <<-EOT
+#     Parameters:
+#       RemediationRoleArn:
+#         Type: String
 
-    Resources:
+#     Resources:
 
-      EbsVolumeUnusedRule:
-        Type: AWS::Config::ConfigRule
-        Properties:
-          ConfigRuleName: ebs-volume-unused
-          Description: Detecta volúmenes EBS en estado available (sin adjuntar)
-          Source:
-            Owner: AWS
-            SourceIdentifier: EC2_VOLUME_INUSE_CHECK
-          Scope:
-            ComplianceResourceTypes:
-              - AWS::EC2::Volume
+#       EbsVolumeUnusedRule:
+#         Type: AWS::Config::ConfigRule
+#         Properties:
+#           ConfigRuleName: ebs-volume-unused
+#           Description: Detecta volúmenes EBS en estado available (sin adjuntar)
+#           Source:
+#             Owner: AWS
+#             SourceIdentifier: EC2_VOLUME_INUSE_CHECK
+#           Scope:
+#             ComplianceResourceTypes:
+#               - AWS::EC2::Volume
 
-      EbsVolumeRemediation:
-        Type: AWS::Config::RemediationConfiguration
-        DependsOn: EbsVolumeUnusedRule
-        Properties:
-          ConfigRuleName: ebs-volume-unused
-          TargetType: SSM_DOCUMENT
-          TargetId: AWS-DeleteEbsVolume
-          TargetVersion: "1"
-          Automatic: true
-          MaximumAutomaticAttempts: 3
-          RetryAttemptSeconds: 60
-          ResourceType: AWS::EC2::Volume
-          Parameters:
-            VolumeId:
-              ResourceValue:
-                Value: RESOURCE_ID
-            AutomationAssumeRole:
-              StaticValue:
-                Values:
-                  - !Ref RemediationRoleArn
-          ExecutionControls:
-            SsmControls:
-              ConcurrentExecutionRatePercentage: 25
-              ErrorPercentage: 20
-  EOT
+#       EbsVolumeRemediation:
+#         Type: AWS::Config::RemediationConfiguration
+#         DependsOn: EbsVolumeUnusedRule
+#         Properties:
+#           ConfigRuleName: ebs-volume-unused
+#           TargetType: SSM_DOCUMENT
+#           TargetId: AWS-DeleteEbsVolume
+#           TargetVersion: "1"
+#           Automatic: true
+#           MaximumAutomaticAttempts: 3
+#           RetryAttemptSeconds: 60
+#           ResourceType: AWS::EC2::Volume
+#           Parameters:
+#             VolumeId:
+#               ResourceValue:
+#                 Value: RESOURCE_ID
+#             AutomationAssumeRole:
+#               StaticValue:
+#                 Values:
+#                   - !Ref RemediationRoleArn
+#           ExecutionControls:
+#             SsmControls:
+#               ConcurrentExecutionRatePercentage: 25
+#               ErrorPercentage: 20
+#   EOT
 
-  input_parameter {
-    parameter_name  = "RemediationRoleArn"
-    parameter_value = aws_iam_role.remediation.arn
-  }
-}
+#   input_parameter {
+#     parameter_name  = "RemediationRoleArn"
+#     parameter_value = aws_iam_role.remediation.arn
+#   }
+# }
