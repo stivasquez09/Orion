16c4d597a189d25a2c4f2ad8b4ef495c9243b9c6
stivasquez09 <stivasquez09@gmail.com>
2026-06-09 20:14:02 -0500

    new aws config + remedetion

--- dev/EBS.tf
+++ dev/EBS.tf
@@ -7,3 +7,13 @@ resource "aws_ebs_volume" "ejemplo_ebs" {
     Name = "Volumen-EBS-Terraform"
   }
 }
+
+resource "aws_ebs_volume" "ejemplo_ebs1" {
+  availability_zone = "us-east-1a" # Reemplaza con tu zona de disponibilidad
+  size              = 20           # Tamaño en GB
+  type              = "gp3"        # Tipo de volumen (gp3, gp2, io1, etc.)
+
+  tags = {
+    Name = "Volumen-EBS-Terraform1"
+  }
+}
